3c392dbd61b72dac15043ab1fa220a788f6e7b90
Flakron <flakron-kadriu@hotmail.com>
2022-04-21 20:23:04 +0200

    resolution fix

--- src/logic/Result/service/Result.ts
+++ src/logic/Result/service/Result.ts
@@ -1,12 +1,6 @@
 import { ResultEnum } from "../model/result-enum";
 
 export class Result {
-  static create(value: ResultEnum) {
-    return {
-      value,
-    };
-  }
-
   static getResultMessage(value: ResultEnum) {
     switch (value) {
       case ResultEnum.DRAW:
